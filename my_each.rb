b1bd2390498b54e05e51fc673b24137f336a87f4
Christian Pinto <pintochristian2@gmail.com>
2019-04-21 15:38:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -7,10 +7,6 @@ def my_each(array) # put argument(s) here
   end
   array
 end
-tas = ['arel', 'jon', 'logan', 'spencer']
-my_each(tas) do |phrase|
-  return phrase
-end
 sayings = ['hi', 'hello', 'bye', 'goodbye']
 my_each(sayings) do |phrase|
   return phrase
